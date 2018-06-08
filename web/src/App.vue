00c9f53e74604a2abdc62a4621fc74b1c6d12ade
Maksim Naranovich <maksim.naranovich@oxagile.com>
2018-06-08 15:37:54 +0300

    FE

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-
+    <b-table striped hover :items="usersTable" :fields="fields"></b-table>
   </div>
 </template>
 
@@ -11,11 +11,21 @@
     name: 'app',
     data: () => ({
       id: null,
-      ws: null
+      ws: null,
+      usersTable: [],
+      fields: [ 'id', 'remote_addr', 'city', 'country', 'cached', 'link' ],
     }),
     beforeMount() {
       this.id = Math.random().toString(36).substr(2, 9)
       this.ws = new WebSocket(Routes.socketPath(this.id))
+      this.ws.addEventListener('message', (e) => {
+        let result = []
+        let data = JSON.parse(e.data)
+        for (let val of Object.values(data)) {
+          result.push(val)
+        }
+        this.usersTable = result
+      })
     }
   }
 </script>
